f1723e87e73f9bc63737c5efbda1ca21569404ec
Claudia I. Falcon <ivette.falcon@gmail.com>
2025-06-13 22:02:33 -0600

    Update deploy with setup

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,11 @@ from app.services.s3 import list_files_by_day
 
 router = APIRouter()
 
+
+@router.get("/")
+def root():
+    return {"message": "API de Riesgo Penalizaciones operando ✅"}
+
 @router.get("/archivos")
 def archivos_disponibles(tipo: str = Query("json")):
     return list_files_by_day(tipo)
